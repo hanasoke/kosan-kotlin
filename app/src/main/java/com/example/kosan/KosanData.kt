8dc860b51771d35bea70b4f9a31109353aaf94b7
hanasbp <hanasoke@gmail.com>
2026-06-05 11:21:30 +0700

    added dorm data

---
+++ app/src/main/java/com/example/kosan/KosanData.kt
@@ -0,0 +1,41 @@
+package com.example.kosan
+
+object KosanData {
+    val list = listOf(
+        Kosan(
+            id = 1,
+            name = "Kosan Mawar",
+            price = "Rp 800.000 / bulan",
+            imageRes = R.drawable.room_1,
+            description = "Kosan nyaman dekat kampus"
+        ),
+        Kosan(
+            id = 2,
+            name = "Kosan Melati",
+            price = "Rp 1.000.000 / bulan",
+            imageRes = R.drawable.room_2,
+            description = "Kosan bersih dan aman"
+        ),
+        Kosan(
+            id = 3,
+            name = "Kosan Anggrek",
+            price = "Rp 1.200.000 / bulan",
+            imageRes = R.drawable.room_3,
+            description = "Kosan premium full fasilitas"
+        ),
+        Kosan(
+            id = 4,
+            name = "Kosan Bola",
+            price = "Rp 1.500.000 / bulan",
+            imageRes = R.drawable.room_4,
+            description = "Kosan untuk anak futsal"
+        ),
+        Kosan(
+            id = 5,
+            name = "Kosan Nuansa Basket",
+            price = "Rp 2.500.000 / bulan",
+            imageRes = R.drawable.room_5,
+            description = "Kosan untuk anak basket"
+        )
+    )
+}
